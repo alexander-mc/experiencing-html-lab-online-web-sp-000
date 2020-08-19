385f1d573ac7f1286419d3fb2dc267ddf074d0ef
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-19 04:17:21 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.failure_color = :teal # or whateer color you like
-  config.success_color = :cyan
+  config.success_color = :teal
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
